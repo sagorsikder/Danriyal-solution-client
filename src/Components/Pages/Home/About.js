1ba26939b1f2b929364597d60482c8e9eac7b28b
sagorsikder <mdsagorsikder97@gmail.com>
2023-03-11 08:32:59 +0600

    polish

--- src/Components/Pages/Home/About.js
+++ src/Components/Pages/Home/About.js
@@ -9,7 +9,7 @@ function AboutUs() {
         <h2 className='text-base mb-16'>Danriyal Solutions Technologies, Bangladesh based custom software development & consulting company focusing on web, mobile, AI, desktop & embedded software development. We always try to provide good service to the client so that they always have atrust in us.</h2>
         </div>
       <div className='lg:grid lg:grid-cols-2 gap-4'>
-        <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 ml-32 lg:ml-0 lg:mr-12 mr-0'>
+        <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-4 ml-32 lg:ml-0 lg:mr-12 mr-0'>
             <div className='h-36 w-44 border-2 items-center text-center pt-8 bg-gray-100 shadow-lg rounded-bl-3xl rounded-tr-3xl'>
                 <h2 className='text-3xl text-black font-bold mb-4'>100+</h2>
                 <h2 className='text-xs'>Active Users</h2>
@@ -32,7 +32,7 @@ function AboutUs() {
             </div>
         </div>
         <div className='mt-8 lg:mt-0'>
-        <h2 className='text-red-500 text-xl mb-3'>Our Story</h2>
+        <h2 className='text-red-500 text-2xl font-semibold mb-3'>Our Story</h2>
         <h2 className='text-4xl font-semibold leading-normal'>A Great Story Starts with a <br/> Friendly Team</h2>
         <h2 className='text-base text-black subpixel-antialiased mt-4'>Our company started with some of our friends. The aim of all of them was to bring the world's best ranking with good service by showing their work skiils.</h2>
         </div>
